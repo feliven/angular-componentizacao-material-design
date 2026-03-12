ab614c811f513998df0e8246631784228a0c8d57
FVS <feliven@gmail.com>
2026-03-12 14:25:22 -0300

    adapta header para telas menores e adiciona box-shadow

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,13 +1,14 @@
 import { Component, output, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatRippleModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
