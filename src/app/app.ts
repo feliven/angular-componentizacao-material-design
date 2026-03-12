911a980c5c7afd01fc0eda58ca60ef0cff48cc74
FVS <feliven@gmail.com>
2026-03-12 15:20:28 -0300

    implementa footer e migra componente de Home para App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,30 @@
-import { Component, DOCUMENT, effect, inject, signal } from '@angular/core';
+import { Component, DOCUMENT, effect, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-componentizacao-material-design');
 
   private readonly document = inject(DOCUMENT);
-
   ativarModoClaro = signal<boolean>(false);
+
+  matIconReg = inject(MatIconRegistry);
 
   constructor() {
     effect(() => {
       this.document.body.style.colorScheme = this.ativarModoClaro() ? 'light' : 'dark';
     });
+  }
+
+  ngOnInit(): void {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
   }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Footer } from '../../shared/footer/footer';
 import { Banner } from '../../shared/banner/banner';
 
 @Component({
   selector: 'app-home',
-  imports: [Footer, Banner],
+  imports: [Banner],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
